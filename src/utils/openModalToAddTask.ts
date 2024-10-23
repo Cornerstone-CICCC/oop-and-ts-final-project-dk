fd38809004701a9ff5769815aa42826fcb0ba383
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-23 14:40:38 -0700

    chore: wip

--- src/utils/openModalToAddTask.ts
+++ src/utils/openModalToAddTask.ts
@@ -1,7 +1,4 @@
-import type { Task } from "../tasks";
-
-export const openModalToAddTask = (taskStatus: Task["status"]) => {
+export const openModalToAddTask = () => {
   const modal = document.querySelector(".modal-add-task") as HTMLElement;
-  modal.id = taskStatus;
   modal.style.display = "block";
 };
